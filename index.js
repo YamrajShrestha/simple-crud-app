273899347f22a173474008a022662ff7a3e96154
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-07-17 18:19:30 +0545

    create product model

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+mongoose
+    .connect("mongodb://localhost:27017/Node-API")
+    .then(() => {
+        console.log("Connected to database");
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+        
+    })
+    .catch(() => {
+        console.log("Connection failed");
+    });
 
 app.get("/", (req, res) => {
     res.send("Hello from Node API");
 });
+
